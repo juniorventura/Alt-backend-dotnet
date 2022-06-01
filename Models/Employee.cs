ae9e66cd9259268c64837cde365dc4af1c696731
jventura <jventura@neural.com.do>
2022-06-01 13:15:08 -0400

    Adding configs to program.cs and workaround for date converter

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace backend_dotnet.Models
 {
@@ -17,7 +18,9 @@ namespace backend_dotnet.Models
         public string FirstName { get; set; } = null!;
         public string? Title { get; set; }
         public string? TitleOfCourtesy { get; set; }
+        [JsonConverter(typeof(DateOnlyConverter))]
         public DateOnly? BirthDate { get; set; }
+        [JsonConverter(typeof(DateOnlyConverter))]
         public DateOnly? HireDate { get; set; }
         public string? Address { get; set; }
         public string? City { get; set; }
@@ -30,11 +33,13 @@ namespace backend_dotnet.Models
         public string? Notes { get; set; }
         public short? ReportsTo { get; set; }
         public string? PhotoPath { get; set; }
-
+        [JsonIgnore]
         public virtual Employee? ReportsToNavigation { get; set; }
+        [JsonIgnore]
         public virtual ICollection<Employee> InverseReportsToNavigation { get; set; }
+        [JsonIgnore]
         public virtual ICollection<Order> Orders { get; set; }
-
+        [JsonIgnore]
         public virtual ICollection<Territory> Territories { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using backend_dotnet;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddTransient<IEmployeeService, EmployeeService>();
+
+// Adding DbContext
+
+builder.Services.AddDbContext<AltDbContext>(op => op.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
 
 var app = builder.Build();
 
